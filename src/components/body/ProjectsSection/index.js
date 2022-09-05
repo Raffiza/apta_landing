4605fbe0e01011e6cb247a380f8501bea474bcf9
Raffiza <raffiza.az@gmail.com>
2022-09-05 14:19:49 +0700

    feat : add projects section

---
+++ src/components/body/ProjectsSection/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { RightContent } from "../AboutUsSection/AboutUsElements";
+
+export const ProjectsSection = () => {
+  return (
+    <ProjectsContainer>
+      <ProjectsH1></ProjectsH1>
+      <ProjectsWrapper>
+        <LeftContent>
+          <TextWrapper>
+            <ProjectsH2>
+
+            </ProjectsH2>
+            <ProjectsP>
+              
+            </ProjectsP>
+          </TextWrapper>
+          <ButtonWrapper>
+            
+          </ButtonWrapper>
+        </LeftContent>
+        <RightContent>
+          <SliderWrapper></SliderWrapper>
+        </RightContent>
+      </ProjectsWrapper>
+    </ProjectsContainer>
+  );
+};
