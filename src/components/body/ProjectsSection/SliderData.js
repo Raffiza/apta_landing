347303b1b4228fde646fe08789d93074db74ef2a
Raffiza <raffiza.az@gmail.com>
2022-09-06 22:46:32 +0700

    feat : add img slider data

---
+++ src/components/body/ProjectsSection/SliderData.js
@@ -0,0 +1,19 @@
+import Slide1 from '../../../images/slide1.png'
+import Slide2 from '../../../images/slide2.png'
+import Slide3 from '../../../images/slide3.png'
+
+export const SliderData = [
+    {
+        image: Slide1
+    },
+    {
+        image: Slide2
+    },
+    {
+        image: Slide3
+
+    }
+]
+    
+    
+    
